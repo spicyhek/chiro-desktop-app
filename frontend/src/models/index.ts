f3ab63636731874acffb1e3cd6e62841f721f9b4
R-Rajaie <ryan.rajaie04@gmail.com>
2025-04-25 00:00:01 -0700

    removed redandadancies

--- frontend/src/models/index.ts
+++
@@ -1,7 +0,0 @@
-import { Insurance } from './Insurance';
-import { Patient } from './Patient';
-import { Doctor } from './Doctor';
-import { Appointment } from './Appointment';
-import { Record } from './Record';
-
-export { Insurance, Patient, Doctor, Appointment, Record }; 
