bf2a20559906622a4ffe4e97048de20d1c14ef7c
spicyhek <59423225+spicyhek@users.noreply.github.com>
2025-05-02 20:21:23 -0700

    patient controller

---
+++ backend/src/main/java/com/chiro/controller/PatientController.java
@@ -0,0 +1,71 @@
+package com.chiro.controller;
+
+import com.chiro.models.Patient;
+import com.chiro.service.PatientService;
+import com.chiro.dao.PatientDAO;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.sql.SQLException;
+import java.util.List;
+
+/*
+Patient controller. Supports HTTP requests POST, GET (patients by ID and all patients), and DELETE.
+ */
+@RestController
+@RequestMapping("/patients")
+public class PatientController {
+
+    private final PatientService patientService;
+
+    public PatientController() {
+        this.patientService = new PatientService(new PatientDAO());
+    }
+
+    @PostMapping
+    public ResponseEntity<?> createPatient(@RequestBody Patient patient) {
+        try {
+            patientService.savePatient(patient);
+            return ResponseEntity.ok("Patient saved successfully");
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body("Validation error: " + e.getMessage());
+        } catch (SQLException e) {
+            return ResponseEntity.internalServerError().body("Database error:" + e.getMessage());
+        }
+
+    }
+
+    @GetMapping
+    public ResponseEntity<?> getAllPatients() {
+        try {
+            List<Patient> patients = patientService.getAllPatients();
+            return ResponseEntity.ok(patients);
+        } catch (SQLException e) {
+            return ResponseEntity.internalServerError().body("Error returning patients:" + e.getMessage());
+        }
+    }
+
+    public ResponseEntity<?> getPatientById(@PathVariable("id") String id) {
+        try {
+            Patient patient = patientService.getPatientById(id);
+            if(patient != null) {
+                return ResponseEntity.ok(patient);
+            } else {
+                return ResponseEntity.notFound().build();
+            }
+        } catch (SQLException e) {
+            return ResponseEntity.internalServerError().body("Error returning patient:" + e.getMessage());
+        }
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<?> deletePatient(@PathVariable("id") String id) {
+        try {
+            patientService.deletePatient(id);
+            return ResponseEntity.ok("Patient deleted successfully");
+        } catch (SQLException e) {
+            return ResponseEntity.internalServerError().body("Could not delete patient: " + e.getMessage());
+        }
+    }
+}
